6340f8dc773d01a30e47ec50dbf27f78410d05d0
nbkhope <ryumemaru@yahoo.com>
2016-12-12 11:12:27 -0800

    Add test for Header component

---
+++ test/components/todos.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from '../../src/components/Header';
+
+// we make a setup() helper that passes the stubbed callbacks as props
+// and renders the component with shallow rendering
+// (This lets individual tests assert on whether the callbacks
+// were called when expected)
+
+function setup() {
+  const props = {
+    addTodo: jest.fn()
+  };
+
+  const enzymeWrapper = shallow(<Header {...props} />);
+
+  return {
+    props,
+    enzymeWrapper
+  };
+}
+
+describe('components', () => {
+  describe('Header', () => {
+    it('should render self and subcomponents', () => {
+      const { enzymeWrapper } = setup();
+
+      expect(enzymeWrapper.find('header').hasClass('header')).toBe(true);
+
+      expect(enzymeWrapper.find('h1').text()).toBe('Todos List');
+
+      const todoInputProps = enzymeWrapper.find('TodoTextInput').props();
+
+      expect(todoInputProps.newTodo).toBe(true)
+      expect(todoInputProps.placeholder).toEqual('What needs to be done?');
+    });
+  });
+});
