d9f257149cabbd3f4bc273d60c47c321451f0fc8
nbkhope <ryumemaru@yahoo.com>
2016-12-11 22:01:38 -0800

    Add test for async action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,27 @@
+import axios from 'axios';
+
 import {
-  ADD_TODO
+  ADD_TODO,
+  FETCH_TODOS_SUCCESS
 } from './types';
+
+const ROOT_URL = 'http://localhost:3090';
 
 export function addTodo(text) {
   return {
     type: ADD_TODO,
     text
   }
+}
+
+export function fetchTodos() {
+  return dispatch => {
+    return axios.get(`${ROOT_URL}/todos`)
+      .then((todos) => {
+        return dispatch({
+          type: FETCH_TODOS_SUCCESS,
+          body: todos
+        })
+      });
+  };
 }
---
+++ test/actions/async.test.js
@@ -0,0 +1,43 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import expect from 'expect';
+
+import * as actions from '../../src/actions';
+import * as types from '../../src/actions/types';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+const ROOT_URL = 'http://localhost:3090';
+
+describe('async actions', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  });
+
+  it('creates FETCH_TODOS_SUCCESS when fetching todos has been done', () => {
+    nock(ROOT_URL)
+      .get('/todos')
+      .reply(200, { body: { todos: ['do something'] } })
+      ;
+
+    const expectedActions = [
+      {
+        type: types.FETCH_TODOS_SUCCESS,
+        body: {
+          todos: ['do something']
+        }
+      }
+    ];
+
+    const store = mockStore({
+      todos: []
+    });
+
+    return store.dispatch(actions.fetchTodos())
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+  });
+});
